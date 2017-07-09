7a7290c0f2bbdbcda21742c81f2bc382e248f886
Andrew Boos <boos1993@gmail.com>
2017-07-08 21:30:46 -0500

    Fix setup script (again)

--- scripts/setup-docker-tls.sh
+++ scripts/setup-docker-tls.sh
@@ -23,8 +23,8 @@ else
   exit 1
 fi
 
-if [[ $EUID -eq 0 ]]; then
-   echo "This script should not be run as root" 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script should be run as root" 
    exit 1
 fi
 
